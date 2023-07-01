e2d203418703c8acd2a33413b72c0fcc7cb3ff2d
Andersson Ambuludi <andersson.ambuludi@unl.edu.ec>
2023-07-01 18:53:57 -0500

    Prueba

---
+++ src/votoelectronico/VotoElectronico.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package votoelectronico;
+
+/**
+ *
+ * @author Andersson
+ */
+public class VotoElectronico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Muchachos");
+    }
+    
+}
