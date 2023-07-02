7a5d61efc95741b097eeb146045b7878bbdc04c2
WilmanMorocho <117868799+WilmanMorocho@users.noreply.github.com>
2023-07-02 17:56:18 -0500

    Se subio avance

---
+++ VotoElectronico/Pis/src/modelo/Persona.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Usuario Asus
+ */
+public class Persona {
+    private Integer id;
+    private String nombre;
+    private String nacionalidad;
+    private String cedula;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+}
---
+++ VotoElectronico/Pis/src/pis/Pis.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pis;
+
+import controlador.dao.PersonaDao;
+import modelo.Persona;
+
+/**
+ *
+ * @author Usuario Asus
+ */
+public class Pis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            PersonaDao p = new PersonaDao();
+            
+            Persona persona = new Persona();
+            persona.setCedula("1106002593");
+            persona.setNacionalidad("Ecuatoriano");
+            persona.setNombre("Wilman");
+            p.setPersona(persona);
+            p.guardar();
+            p.listar().imprimir();
+            
+            Persona persona1 = new Persona();
+            persona.setCedula("1106012345");
+            persona.setNacionalidad("Ecuatoriano");
+            persona.setNombre("Juan");
+            p.setPersona(persona);
+            p.guardar();
+            p.listar().imprimir();
+
+        } catch (Exception e) {
+        }
+
+    }
+
+}
